2ec92f6ff9fade69097d30b65ba1c597d191367d
Regan Murphy <rmurphy@microsoft.com>
2018-08-01 11:08:15 +1200

    added a yaml file that can be used to deploy container in kubernetes updated customstart shell script for correct use of "" in produced appsettings.json file from passed ENV vars

--- DataGenNetCore/customstart.sh
+++ DataGenNetCore/customstart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo \{"EventHubConnectionString":"$EVENTHUBCONNECTIONSTRING","EventHubPath":"$EVENTHUB"\} > appsettings.json
+echo "{\"EventHubConnectionString\":\"$EVENTHUBCONNECTIONSTRING\",\"EventHubPath\":\"$EVENTHUB\"}" > appsettings.json
 
 if [[ -z "$VERBOSE" ]]; then
     dotnet DataGenNetCore.dll sendmessages --message-type telco $VELOCITY
